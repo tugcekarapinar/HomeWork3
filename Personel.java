fffc2f3c2b47cdd4712ca120adb85147c5a7978e
tugcekarapinar <101087196+tugcekarapinar@users.noreply.github.com>
2024-05-19 11:44:55 +0300

    personel.java added

---
+++ Personel.java
@@ -0,0 +1,10 @@
+public class Personel {
+    public String ad;
+    public String soyad;
+    public int yas;
+    public String adres;
+    public double maas;
+    public String telefonNumarasi;
+    public boolean evliMi;
+}
+
